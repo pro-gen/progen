6c5adc0a049b8b8644413549cae96fa9b5875243
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-07-13 17:50:09 +0200

    Formateo de código

--- progen/src/main/java/progen/kernel/functions/Terminal.java
+++ progen/src/main/java/progen/kernel/functions/Terminal.java
@@ -18,53 +18,52 @@ import progen.userprogram.UserProgram;
  */
 public abstract class Terminal extends Function {
 
-    /** Valor concreto del terminal del árbol. */
-    private Object value;
+  /** Valor concreto del terminal del árbol. */
+  private Object value;
 
-    /**
-     * Constructor por defecto que recibe dos parámetros y llama al constructor
-     * de la clase padre
-     * 
-     * @see Function
-     * @param signature
-     *            valor de retorno del terminal.
-     * @param symbol
-     *            con el que se representará el terminal.
-     */
-    public Terminal(String signature, String symbol) {
-	super(signature, symbol);
-	value = null;
-    }
+  /**
+   * Constructor por defecto que recibe dos parámetros y llama al constructor de
+   * la clase padre
+   * 
+   * @see Function
+   * @param signature
+   *          valor de retorno del terminal.
+   * @param symbol
+   *          con el que se representará el terminal.
+   */
+  public Terminal(String signature, String symbol) {
+    super(signature, symbol);
+    value = null;
+  }
 
-    /**
-     * Define el valor que tendrá dicho terminal.
-     * 
-     * @param value
-     *            el valor del terminal.
-     */
-    public void setValue(Object value) {
-	this.value = value;
-    }
+  /**
+   * Define el valor que tendrá dicho terminal.
+   * 
+   * @param value
+   *          el valor del terminal.
+   */
+  public void setValue(Object value) {
+    this.value = value;
+  }
 
-    /**
-     * Devuelve el valor concreto del terminal.
-     * 
-     * @return el valor concreto del terminal.
-     */
-    public Object getValue() {
-	return value;
-    }
+  /**
+   * Devuelve el valor concreto del terminal.
+   * 
+   * @return el valor concreto del terminal.
+   */
+  public Object getValue() {
+    return value;
+  }
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see progen.kernel.functions.Terminal#evaluate(java.util.List,
-     * progen.userprogram.UserProgram, java.util.HashMap)
-     */
-    @Override
-    public Object evaluate(List<Node> arguments, UserProgram userProgram,
-	    HashMap<String, Object> returnAddr) {
-	return getValue();
-    }
+  /*
+   * (non-Javadoc)
+   * 
+   * @see progen.kernel.functions.Terminal#evaluate(java.util.List,
+   * progen.userprogram.UserProgram, java.util.HashMap)
+   */
+  @Override
+  public Object evaluate(List<Node> arguments, UserProgram userProgram, HashMap<String, Object> returnAddr) {
+    return getValue();
+  }
 
 }
