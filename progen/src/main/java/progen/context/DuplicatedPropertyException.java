94632181e3dec66a5c2e159c9dea8c8f68599a3f
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-07-14 18:05:42 +0200

    Correcciones de checkstyle

--- progen/src/main/java/progen/context/DuplicatedPropertyException.java
+++ progen/src/main/java/progen/context/DuplicatedPropertyException.java
@@ -3,26 +3,31 @@ package progen.context;
 import progen.kernel.error.Error;
 
 /**
- * Excepción que es lanzada cuando se detecte que hay propiedades definidas varias veces 
- * en los distintos ficheros de configuración.
+ * Excepción que es lanzada cuando se detecte que hay propiedades definidas
+ * varias veces en los distintos ficheros de configuración.
  * 
- * @author jirsis 
- *
+ * @author jirsis
+ * 
  */
 public class DuplicatedPropertyException extends RuntimeException {
 
-	private static final long serialVersionUID = 2058933988829590605L;
+  private static final int ID_ERROR = 17;
+  
+  private static final long serialVersionUID = 2058933988829590605L;
 
-	public DuplicatedPropertyException() {
-      super(Error.get(17).trim());
+  public DuplicatedPropertyException() {
+    super(Error.get(ID_ERROR).trim());
   }
-	
-	/**
-	 * Constructor de la clase. Recibe el nombre de la propiedad que está duplicada.
-	 * @param message Propiedad duplicada.
-	 */
-	public DuplicatedPropertyException(String message) {
-		super(Error.get(17).trim()+ "("+message+")");
-	}
+
+  /**
+   * Constructor de la clase. Recibe el nombre de la propiedad que está
+   * duplicada.
+   * 
+   * @param message
+   *          Propiedad duplicada.
+   */
+  public DuplicatedPropertyException(String message) {
+    super(Error.get(ID_ERROR).trim() + "(" + message + ")");
+  }
 
 }
