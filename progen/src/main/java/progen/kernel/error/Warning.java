cb528e6d13ac0475fee588cac89085fbfbc71a70
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-10-11 19:29:30 +0200

    Refactor de variable

--- progen/src/main/java/progen/kernel/error/Warning.java
+++ progen/src/main/java/progen/kernel/error/Warning.java
@@ -25,23 +25,23 @@ public final class Warning {
    * Muetra por pantalla un aviso, al que es necesario proporcionarle el código
    * de aviso y el mensaje personalizado.
    * 
-   * @param id
+   * @param idWarning
    *          Identificador del aviso
    * @param msg
    *          Mensaje personalizado
    */
-  public static void show(int id, String msg) {
-    System.out.println(literals.getString(WARNING_LITERAL + id) + "[" + msg + "]");
+  public static void show(int idWarning, String msg) {
+    System.out.println(literals.getString(WARNING_LITERAL + idWarning) + "[" + msg + "]");
   }
 
   /**
    * Muetra por pantalla un aviso, al que es necesario proporcionarle el código
    * de aviso.
    * 
-   * @param id
+   * @param idWarning
    *          Identificador del aviso
    */
-  public static void show(int id) {
-    System.out.println(literals.getString(WARNING_LITERAL + id));
+  public static void show(int idWarning) {
+    System.out.println(literals.getString(WARNING_LITERAL + idWarning));
   }
 }
