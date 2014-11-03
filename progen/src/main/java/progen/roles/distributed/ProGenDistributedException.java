ab90ef8c381c0787b9fdcaadfc4b2b1a44a95c4d
Iñaki Reta Sabarrós <ignacio.reta@gmail.com>
2014-11-03 21:38:34 +0100

    Eliminado info de checkstyle

--- progen/src/main/java/progen/roles/distributed/ProGenDistributedException.java
+++ progen/src/main/java/progen/roles/distributed/ProGenDistributedException.java
@@ -13,7 +13,7 @@ public class ProGenDistributedException extends RuntimeException {
   public ProGenDistributedException() {
     super(Error.get(ID_ERROR));
   }
-  
+
   public ProGenDistributedException(Exception originalException) {
     super(Error.get(ID_ERROR), originalException);
   }
@@ -21,17 +21,17 @@ public class ProGenDistributedException extends RuntimeException {
   public ProGenDistributedException(String msg) {
     super(getMessage(ID_ERROR, msg));
   }
-  
+
   public ProGenDistributedException(String msg, Exception originalException) {
     super(getMessage(ID_ERROR, msg), originalException);
   }
 
-  private static String getMessage(int idError, String msg) {
-    return Error.get(idError) + BLANK_SPACE_SYMBOL+SQUARE_LEFT_BRACKET_SYMBOL + msg + SQUARE_RIGHT_BRACKET_SYMBOL;
-  }
-
   public ProGenDistributedException(int id, String msg) {
     super(getMessage(id, msg));
+  }
+
+  private static String getMessage(int idError, String msg) {
+    return Error.get(idError) + BLANK_SPACE_SYMBOL + SQUARE_LEFT_BRACKET_SYMBOL + msg + SQUARE_RIGHT_BRACKET_SYMBOL;
   }
 
 }
