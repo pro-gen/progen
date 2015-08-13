be0c5e04ef6bce1d3ef4c983ab75c69825ff86b0
Iñaki Reta Sabarrós <jirsis@gmail.com>
2015-08-13 22:18:48 +0200

    Añadido el test de StandardCrossover

--- progen/src/test/java/progen/kernel/evolution/operators/StandardCrossoverTest.java
+++ progen/src/test/java/progen/kernel/evolution/operators/StandardCrossoverTest.java
@@ -1,15 +1,29 @@
 package progen.kernel.evolution.operators;
 
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
 import progen.context.ProGenContext;
+import progen.kernel.evolution.selector.NullSelector;
+import progen.kernel.evolution.selector.RandomSelector;
+import progen.kernel.evolution.selector.Selector;
+import progen.kernel.grammar.Grammar;
 import progen.kernel.population.Individual;
 import progen.kernel.population.Population;
 
@@ -17,63 +31,77 @@ import progen.kernel.population.Population;
  * @author jirsis
  * @since 2.0
  */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ProGenContext.class, Selector.class})
 public class StandardCrossoverTest {
-  /**
-   * Referencia al operador de cruce
-   */
   private StandardCrossover crossover;
-  /**
-   * Referencia a la población a cruzar.
-   */
+
   private Population population;
 
-  /**
-   * @throws java.lang.Exception
-   */
   @Before
   public void setUp() throws Exception {
-    ProGenContext.makeInstance();
-    ProGenContext.setProperty("progen.total.RPB", 1 + "");
-    ProGenContext.setProperty("progen.RPB0.functionSet", 0 + "");
-    ProGenContext.setProperty("progen.functionSet0", "BitAndFc, BitOrFc, BitXor, BitNotFc, BitMultFc, BitSumFc, BitVrotdFc, A0, Hval, Bit32ERC");
-    ProGenContext.setProperty("progen.functionSet0.return", "Integer");
-    ProGenContext.setProperty("progen.experiment.file", "app.gpHashDyn.gpHashDyn.txt");
-    ProGenContext.setProperty("progen.population.init-mode", "half&half$$grow=50%,full=0.5");
-    ProGenContext.setProperty("progen.population.init-depth-interval", "2,10");
-
     crossover = new StandardCrossover();
-    Map<String, String> params = ProGenContext.getParameters("default.params");
-    crossover.setSelector("RandomSelector", params);
-    population = new Population(10);
+    crossover.setSelector("RandomSelector", null);
 
+    mockProGenContext();
+    mockPopulation();
   }
 
-  @Test@Ignore
-  public void testEvolveRandomInit() {
-    List<Individual> evolucion;
-    evolucion = crossover.evolve(population);
-    for (int i = 0; i < evolucion.size(); i++) {
-      for (int j = 0; j < population.getIndividuals().size(); j++) {
-        assertFalse(population.getIndividual(j).toString().equals(evolucion.get(i).toString()));
-      }
-    }
+  @Test
+  public void testConstructor() {
+    crossover = new StandardCrossover();
+    assertEquals(0.0, crossover.getProbability(), 0.1);
+    assertTrue(crossover.getSelector() instanceof NullSelector);
   }
 
-  @Test@Ignore
-  public void testEvolveGrowInit() {
-    ProGenContext.setProperty("progen.population.init-mode", "grow");
-    List<Individual> evolucion;
-    evolucion = crossover.evolve(population);
-    for (int i = 0; i < evolucion.size(); i++) {
-      for (int j = 0; j < population.getIndividuals().size(); j++) {
-        assertFalse(population.getIndividual(j).toString().equals(evolucion.get(i).toString()));
-      }
-    }
+  @Test
+  public void testEvolve() {
+    List<Individual> evolucion = crossover.evolve(population);
+    assertEquals(2, evolucion.size());
+    assertNotEquals(population.getIndividual(0), evolucion.get(0));
+    assertNotEquals(population.getIndividual(1), evolucion.get(1));
+  }
+  
+  @Test(expected = SelectorSizeIncorrectValueException.class)
+  public void invalidSelector(){
+    mockStatic(Selector.class);
+    RandomSelector selector = mock(RandomSelector.class);
+    when(ProGenContext.getOptionalProperty("progen.language", "en_US")).thenReturn("en_US");
+    when(selector.select(population, 2)).thenReturn(new ArrayList<Individual>());
+    when(Selector.makeSelector("RandomSelector", null)).thenReturn(selector);
+    crossover.setSelector("RandomSelector", null);
+    crossover.evolve(population);
   }
 
-  @Test@Ignore
-  public void testDepth10() {
-    ProGenContext.setProperty("progen.population.init-depth-interval", "10");
-    testEvolveGrowInit();
+  private void mockPopulation() {
+    population = mock(Population.class);
+    List<Individual> individuals = mockIndividuals();
+    when(population.getIndividuals()).thenReturn(individuals);
+    when(population.getIndividual(any(Integer.class))).thenReturn(individuals.get(0));
   }
+
+  private void mockProGenContext() {
+    mockStatic(ProGenContext.class);
+    ProGenContext context = mock(ProGenContext.class);
+    when(ProGenContext.makeInstance(any(String.class))).thenReturn(context);
+    when(ProGenContext.getMandatoryProperty("progen.total.RPB")).thenReturn("1");
+    when(ProGenContext.getMandatoryProperty("progen.RPB0.functionSet")).thenReturn("0");
+    when(ProGenContext.getMandatoryProperty("progen.functionSet0.return")).thenReturn("int");
+    when(ProGenContext.getMandatoryProperty("progen.functionSet0")).thenReturn("IntMult, IntMinus, IntN");
+    when(ProGenContext.getOptionalProperty("progen.user.home", "progen.kernel.functions.")).thenReturn("progen.kernel.functions.");
+    when(ProGenContext.getOptionalProperty("progen.total.RPB", 1)).thenReturn(1);
+    when(ProGenContext.getOptionalProperty("progen.population.init-mode", "half&half")).thenReturn("full");
+    when(ProGenContext.getOptionalProperty("progen.population.max-nodes", Integer.MAX_VALUE)).thenReturn(Integer.MAX_VALUE);
+    when(ProGenContext.getOptionalProperty("progen.max-attempts", 100)).thenReturn(10);
+    when(ProGenContext.getMandatoryProperty("progen.population.init-depth-interval")).thenReturn("2,10");
+  }
+
+  private List<Individual> mockIndividuals() {
+    List<Individual> population = new ArrayList<Individual>();
+    Map<String, Grammar> grammars = new HashMap<String, Grammar>();
+    grammars.put("RPB0", new Grammar("RPB0"));
+    population.add(new Individual(grammars));
+    return population;
+  }
+
 }
